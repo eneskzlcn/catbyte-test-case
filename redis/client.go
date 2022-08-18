1d3a6179a224a5cb2e53524a3833e02703718611
Enes Kızılcan <eneskzlcn@Eness-Air.home>
2022-08-18 18:06:18 +0300

    redis client and its tests done

--- redis/client.go
+++ redis/client.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"encoding/json"
 	"github.com/go-redis/redis"
+	"log"
 )
 
 type Client struct {
@@ -10,7 +11,6 @@ type Client struct {
 }
 
 func NewClient(address, password string) *Client {
-
 	client := redis.NewClient(&redis.Options{
 		Addr:     address,
 		Password: password,
@@ -24,9 +24,26 @@ func NewClient(address, password string) *Client {
 func (c *Client) SaveStruct(key string, value interface{}) error {
 	jsonValue, err := json.Marshal(value)
 	if err != nil {
+		log.Println("marshalling error")
 		return err
 	}
 	if err = c.client.Set(key, jsonValue, 0).Err(); err != nil {
+		log.Println("redis set error")
+		return err
+	}
+	return nil
+}
+
+//SaveToArrayL adds new elements to the beginning of the array as the task wants to.
+
+func (c *Client) SaveToArrayL(key string, value interface{}) error {
+	jsonValue, err := json.Marshal(value)
+	if err != nil {
+		log.Println("marshalling error")
+		return err
+	}
+	if err = c.client.LPush(key, jsonValue).Err(); err != nil {
+		log.Println("redis set error")
 		return err
 	}
 	return nil
@@ -37,6 +54,23 @@ func (c *Client) GetStruct(key string, out interface{}) error {
 		return err
 	}
 	if err = json.Unmarshal(val, &out); err != nil {
+		return err
+	}
+	return nil
+}
+func (c *Client) GetArray(key string, out *[]string) error {
+	val, err := c.client.LRange(key, 0, -1).Result()
+	if err != nil {
+		return err
+	}
+	*out = val
+	for _, item := range val {
+		println(item)
+	}
+	return nil
+}
+func (c *Client) Delete(key string) error {
+	if err := c.client.Del(key).Err(); err != nil {
 		return err
 	}
 	return nil
--- redis/client_test.go
+++ redis/client_test.go
@@ -1,1 +1,144 @@
-package redis
+package redis_test
+
+import (
+	"github.com/eneskzlcn/catbyte-test-task/redis"
+	"github.com/goccy/go-json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewClient(t *testing.T) {
+	//redis image should be up and serve on 6379 to pass that test.
+	client := redis.NewClient("localhost:6379", "")
+	assert.NotNil(t, client)
+}
+func TestClient_SaveStruct(t *testing.T) {
+	type mockType struct {
+		Name    string   `json:"name"`
+		Surname string   `json:"surname"`
+		Hobbies []string `json:"hobbies"`
+	}
+	mockData := mockType{
+		Name:    "enes",
+		Surname: "kzlcn",
+		Hobbies: []string{"football", "swimming", "listening music"},
+	}
+	client := redis.NewClient("localhost:6379", "")
+	assert.NotNil(t, client)
+	err := client.SaveStruct(mockData.Name, mockData)
+	assert.Nil(t, err)
+}
+func TestClient_GetStruct(t *testing.T) {
+	// post a new mockData to get
+	type mockType struct {
+		Name    string   `json:"name"`
+		Surname string   `json:"surname"`
+		Hobbies []string `json:"hobbies"`
+	}
+	mockData := mockType{
+		Name:    "enes",
+		Surname: "kzlcn",
+		Hobbies: []string{"football", "swimming", "listening music"},
+	}
+	client := redis.NewClient("localhost:6379", "")
+	assert.NotNil(t, client)
+	err := client.SaveStruct(mockData.Name, mockData)
+	assert.Nil(t, err)
+	// test getting the data.
+	mockDataResult := mockType{}
+	err = client.GetStruct(mockData.Name, &mockDataResult)
+	assert.Nil(t, err)
+	assert.Equal(t, mockData.Name, mockDataResult.Name)
+	assert.Equal(t, mockData.Surname, mockDataResult.Surname)
+	assert.ElementsMatch(t, mockData.Hobbies, mockDataResult.Hobbies)
+}
+func TestClient_SaveToArrayL(t *testing.T) {
+	// post a new mockData to get
+	type mockType struct {
+		Name    string   `json:"name"`
+		Surname string   `json:"surname"`
+		Hobbies []string `json:"hobbies"`
+	}
+	mockKey := "mock"
+	mockDatas := []mockType{
+		{
+			Name:    "1",
+			Surname: "k",
+			Hobbies: []string{"basketball"},
+		},
+		{
+			Name:    "2",
+			Surname: "k",
+			Hobbies: []string{"basketball"},
+		},
+		{
+			Name:    "3",
+			Surname: "k",
+			Hobbies: []string{"basketball"},
+		},
+		{
+			Name:    "4",
+			Surname: "k",
+			Hobbies: []string{"basketball"},
+		},
+	}
+	client := redis.NewClient("localhost:6379", "")
+	assert.NotNil(t, client)
+	for _, mockData := range mockDatas {
+		err := client.SaveToArrayL(mockKey, mockData)
+		assert.Nil(t, err)
+	}
+
+}
+func TestClient_GetArray(t *testing.T) {
+	// post a new mockData to get
+	type mockType struct {
+		Name    string   `json:"name"`
+		Surname string   `json:"surname"`
+		Hobbies []string `json:"hobbies"`
+	}
+	mockKey := "mock"
+	mockDatas := []mockType{
+		{
+			Name:    "1",
+			Surname: "k",
+			Hobbies: []string{"basketball"},
+		},
+		{
+			Name:    "2",
+			Surname: "k",
+			Hobbies: []string{"basketball"},
+		},
+		{
+			Name:    "3",
+			Surname: "k",
+			Hobbies: []string{"basketball"},
+		},
+		{
+			Name:    "4",
+			Surname: "k",
+			Hobbies: []string{"basketball"},
+		},
+	}
+	client := redis.NewClient("localhost:6379", "")
+	assert.NotNil(t, client)
+	//clear mock key
+	err := client.Delete(mockKey)
+	defer client.Delete(mockKey)
+	assert.Nil(t, err)
+	for _, mockData := range mockDatas {
+		err := client.SaveToArrayL(mockKey, mockData)
+		assert.Nil(t, err)
+	}
+	// try to get data into a type
+	resultStr := make([]string, 0)
+	err = client.GetArray(mockKey, &resultStr)
+	assert.Nil(t, err)
+
+	mockResult := make([]mockType, len(resultStr))
+	for index, item := range resultStr {
+		err = json.Unmarshal([]byte(item), &mockResult[index])
+		assert.Nil(t, err)
+	}
+	assert.ElementsMatch(t, mockDatas, mockResult)
+}
