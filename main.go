e51d0f6475df0a24653c9db9fe51eb616ea6b36f
Enes Kızılcan <eneskzlcn@Eness-Air.home>
2022-08-18 18:22:45 +0300

    main feeded with config ops and reporting service

--- main.go
+++ main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"github.com/eneskzlcn/catbyte-test-task/internal/config"
 	"github.com/eneskzlcn/catbyte-test-task/internal/message"
 	"github.com/eneskzlcn/catbyte-test-task/internal/processor"
+	"github.com/eneskzlcn/catbyte-test-task/internal/reporting"
 	"github.com/eneskzlcn/catbyte-test-task/rabbitmq"
 	"github.com/eneskzlcn/catbyte-test-task/redis"
-	server "github.com/eneskzlcn/catbyte-test-task/server"
+	"github.com/eneskzlcn/catbyte-test-task/server"
 	"log"
+	"os"
 )
 
 func main() {
-	rabbitMQClient := rabbitmq.New("amqp://guest:guest@localhost:6001/", "catbyte-messages")
-	redisClient := redis.NewClient("localhost:6379", "")
+	//does not in any other environment (prod, qa or etc.) yet. So directly load local config.
+	config, err := config.LoadConfig(".dev/", "local", "yaml")
+	if err != nil {
+		return
+	}
 
+	rabbitMQClient := rabbitmq.New(config.RabbitMQ.Address, config.RabbitMQ.Queue)
+	redisClient := redis.NewClient(config.Redis.Address, config.Redis.Password)
+
+	//app 1: message api
 	messageService := message.NewService(rabbitMQClient)
 	messageHandler := message.NewHandler(messageService)
 
+	//app 2: processor
 	processorService := processor.NewService(rabbitMQClient, redisClient)
-
 	go processorService.StartProcessing()
+
+	//app 3: reporting
+	reportingService := reporting.NewService(redisClient)
+	reportingHandler := reporting.NewHandler(reportingService)
 
 	server := server.NewServer([]server.Handler{
 		messageHandler,
-	}, []string{"*"}, "3000")
+		reportingHandler,
+	}, config.Server.TrustedProxies, config.Server.Port)
 
-	err := server.Start()
-	if err != nil {
+	if err = server.Start(); err != nil {
 		log.Println("server closed with error")
+		os.Exit(1)
 	}
 }
